73c9471f56232cd1299e8b55b28797b9026acb0d
Alvaro Pereda <alvaro.pereda@gmail.com>
2014-04-06 21:23:41 +0100

    Client app added.

---
+++ NewsClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using NewsAPI.Models;
+
+namespace NewsClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:26118/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                 HttpResponseMessage response = await client.GetAsync("api/users/27");
+                if (response.IsSuccessStatusCode)
+                {
+                    User user = await response.Content.ReadAsAsync<User>();
+                    Console.WriteLine("{0}", user.Name);
+                }
+            }
+        }
+    }
+}
